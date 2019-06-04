858b20b2d008b5f18706f1ecf3e58ffdfc52f91c
farhad keyvan ghazvini <farhadkeyvanghazvini@macbook-pro.local>
2019-06-04 10:46:43 +0300

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by farhad keyvan ghazvini on 6/4/19.
+//  Copyright © 2019 farhad keyvan ghazvini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
